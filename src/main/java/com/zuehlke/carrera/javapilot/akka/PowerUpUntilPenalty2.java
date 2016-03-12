e31f371f6706493dcf8f4fcb0daece9661a98dd5
BugMan42 <guillem812@gmail.com>
2016-03-12 14:39:51 +0100

    Power Up Until 2: The beginning

---
+++ src/main/java/com/zuehlke/carrera/javapilot/akka/PowerUpUntilPenalty2.java
@@ -0,0 +1,116 @@
+package com.zuehlke.carrera.javapilot.akka;
+
+import akka.actor.ActorRef;
+import akka.actor.Props;
+import akka.actor.UntypedActor;
+import com.zuehlke.carrera.relayapi.messages.PenaltyMessage;
+import com.zuehlke.carrera.relayapi.messages.RaceStartMessage;
+import com.zuehlke.carrera.relayapi.messages.SensorEvent;
+import com.zuehlke.carrera.timeseries.FloatingHistory;
+import org.apache.commons.lang.StringUtils;
+
+/**
+ *  this logic node increases the power level by 10 units per 0.5 second until it receives a penalty
+ *  then reduces by ten units.
+ */
+public class PowerUpUntilPenalty2 extends UntypedActor {
+
+    private final ActorRef kobayashi;
+
+    private double currentPower = 0;
+
+    private long lastIncrease = 0;
+
+    private int maxPower = 180; // Max for this phase;
+
+    private boolean probing = true;
+
+    private FloatingHistory gyrozHistory = new FloatingHistory(8);
+
+    /**
+     * @param pilotActor The central pilot actor
+     * @param duration the period between two increases
+     * @return the actor props
+     */
+    public static Props props( ActorRef pilotActor, int duration ) {
+        return Props.create(
+                PowerUpUntilPenalty.class, () -> new PowerUpUntilPenalty(pilotActor, duration ));
+    }
+    private final int duration;
+
+    public PowerUpUntilPenalty2(ActorRef pilotActor, int duration) {
+        lastIncrease = System.currentTimeMillis();
+        this.kobayashi = pilotActor;
+        this.duration = duration;
+    }
+
+
+    @Override
+    public void onReceive(Object message) throws Exception {
+
+        if ( message instanceof SensorEvent ) {
+            handleSensorEvent((SensorEvent) message);
+
+        } else if ( message instanceof PenaltyMessage) {
+            handlePenaltyMessage ();
+
+        } else if ( message instanceof RaceStartMessage) {
+            handleRaceStart();
+
+        } else {
+            unhandled(message);
+        }
+    }
+
+    private void handleRaceStart() {
+        currentPower = 0;
+        lastIncrease = 0;
+        maxPower = 180; // Max for this phase;
+        probing = true;
+        gyrozHistory = new FloatingHistory(8);
+    }
+
+    private void handlePenaltyMessage() {
+        currentPower -= 10;
+        kobayashi.tell(new PowerAction((int)currentPower), getSelf());
+        probing = false;
+    }
+
+    /**
+     * Strategy: increase quickly when standing still to overcome haptic friction
+     * then increase slowly. Probing phase will be ended by the first penalty
+     * @param message the sensor event coming in
+     */
+    private void handleSensorEvent(SensorEvent message) {
+
+        double gyrz = gyrozHistory.shift(message.getG()[2]);
+        show ((int)gyrz);
+
+        if (probing) {
+            if (iAmStillStanding()) {
+                increase(0.5);
+            } else if (message.getTimeStamp() > lastIncrease + duration) {
+                lastIncrease = message.getTimeStamp();
+                increase(3);
+            }
+        }
+
+        kobayashi.tell(new PowerAction((int)currentPower), getSelf());
+    }
+
+    private int increase ( double val ) {
+        currentPower = Math.min ( currentPower + val, maxPower );
+        return (int)currentPower;
+    }
+
+    private boolean iAmStillStanding() {
+        return gyrozHistory.currentStDev() < 3;
+    }
+
+    private void show(int gyr2) {
+        int scale = 120 * (gyr2 - (-10000) ) / 20000;
+        System.out.println(StringUtils.repeat(" ", scale) + gyr2);
+    }
+
+
+}
